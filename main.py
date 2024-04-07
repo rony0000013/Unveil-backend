e8530585475b7989693e3cbb515c1f6ebf11db5a
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2024-04-08 00:35:21 +0530

    cors fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, requests
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import yfinance as yf
 
 import redis
@@ -27,6 +28,14 @@ The company information: "
    
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @rediscache.cache(ttl=86400)
 def fetch_news(ticker:str):
